13aa436814c325b2663abffe8fe626058710097c
Mr.Mean <barzi1375@gmail.com>
2016-03-10 22:20:37 +0330

    The exception class NoPermission has been added, rename method of the File class has been added, now raising errors will happen in the __init__ of the File Class

--- __Classes.py
+++ __Classes.py
@@ -1,15 +1,18 @@
 from os import rename, access, R_OK, F_OK
 from ErrorHandler import error_show
-from exceptions import FileNotExist
+from exceptions import FileNotExist, NoPermission
 
 
 class File(object):
     def __init__(self, strAdrs):
         self.fullPath = strAdrs
+        self.__existence()
+        self.__reachable()
         self.type = strAdrs.split(".")[-1]
         self.name = strAdrs.split("\\")[-1]
         self.Jname = strAdrs.name.split(".")[0]
         self.parent = strAdrs.split("\\")[-2]
+
 
     def __existence(self):
         """
@@ -18,6 +21,10 @@ class File(object):
         """
         if not access(self.fullPath, F_OK):
             raise FileNotExist
+
+    def __reachable(self):
+        if not access(self.fullPath, R_OK):
+            raise NoPermission
 
     def openf(self):
         pass
@@ -28,29 +35,34 @@ class File(object):
     def cut(self, second_path):
         """
         | This method moves a file from current directory to the second directory.
-        | If the file has not exist, a FileNotExist exception raise and handle.
-        | If the second path has not exist, WindowsError exception will handle.
         :param self : Object
         :param second_path: str
         """
         try:
-            self.__existence()
             rename(self.fullPath, second_path)
         except WindowsError:
             error_show('The second directory path is invalid', 'listener must add')
-        except FileNotExist as error:
-            error_show(str(error), 'listener must add')
         except Exception:
             error_show("An Error happened, please restart the app.", 'listener must add')
 
     def delete(self):
         pass
 
-    def rename(self,newName):
-        pass
+    def rename(self, new_name):
+        """
+        | This method renames current file
+        :param self: Obj
+        :param new_name:str
+        """
+        try:
+            rename(self.fullPath, ''.join(self.fullPath[:len(self.fullPath) - 1]) + new_name)
+        except WindowsError:
+            error_show('The second directory path is invalid', 'listener must add')
+        except Exception:
+            error_show("An Error happened, please restart the app.", 'listener must add')
 
 
-class directory(object):
+class Directory(object):
     def __init__(self, strAdrs):
         self.fullAddress = strAdrs
         self.name = strAdrs.split("\\")[-1]
--- exceptions.py
+++ exceptions.py
@@ -6,8 +6,30 @@ Exceptions list:
 
 
 class FileNotExist(Exception):
+    """
+    | This error rise When file has not exist.This class Inherit from Exception class.
+    | The __str__ method of The Exception class has been overridden.
+    """
     def __init__(self):
         super(FileNotExist).__init__(self)
 
     def __str__(self):
+        """
+        :return "File not exist" : str
+        """
         return "File not exist"
+
+
+class NoPermission(Exception):
+    """
+    | This error rise When the file is not readable.This class Inherit from Exception class.
+    | The __str__ method of The Exception class has been overridden.
+    """
+    def __init__(self):
+        super(NoPermission).__init__(self)
+
+    def __str__(self):
+        """
+        :return "You have not right permission to change this file" : str
+        """
+        return "You have not right permission to change this file"
