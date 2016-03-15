d95708f91521b3cfb7d6386bbf7affcb55fe9675
Mr.Mean <barzi1375@gmail.com>
2016-03-15 11:53:46 +0330

    create_cash_table and add_new_file functions has been added to the db.py file .

--- db.py
+++ db.py
@@ -15,7 +15,32 @@ def connect_db():
 
 
 def create_cash_table():
-    connection = connect_db()
-    cursor = connection.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS cash(id INT AUTO INCREMENT UNSIGNED PRIMARY KEY,fod_name VARCHAR(255),'
-                   'PARENT TEXT, kind TINYINT(1))')
+    """
+    | This void function creates cash table if it not exists.
+    create_cash_table()
+    """
+    connection_obj = connect_db()
+    cursor = connection_obj.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS cash(id INTEGER AUTOINCREMENT UNSIGNED PRIMARY KEY,fod_name INT(5),'
+                   'parent TEXT,kind TINYINT(1),fod_size INT UNSIGNED,Ctime DATETIME,mode CHAR(10),cash_date DATETIME)')
+    connection_obj.commit()
+    connection_obj.close()
+
+
+def add_new_file(file_name, parent_path, size, create_time, file_mode, cash_date):
+    """
+    | This void function adds a new file to the cash table.
+    :param file_name:str
+    :param parent_path:str
+    :param size:int
+    :param create_time:str
+    :param file_mode :int
+    :param cash_date:str
+    """
+    connection_obj = connect_db()
+    cursor = connection_obj.cursor()
+    query = ((file_name, parent_path, 1, size, create_time, file_mode, cash_date),)
+    cursor.executemany("INSERT INTO cash(fod_name,parent,kind,fod_size,Ctime,mode,cash_date) VALUES(?,?,?,?,?,?,?)", query)
+    connection_obj.commit()
+    connection_obj.close()
+
