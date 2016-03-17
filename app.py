2ecc32dfac4b6c850b7e26088bda6aab304feb15
Mr.Mean <barzi1375@gmail.com>
2016-03-17 19:41:30 +0330

    Ali B help.pdf file has been added. events.py file has been added

--- app.py
+++ app.py
@@ -2,37 +2,40 @@ from __Classes import *
 from DefaultUI import *
 from funcs import *
 from visual import *
+from navigation import *
+add_here('\\')
+
 
 def window():
     # This function is the main window of the application.
-   app = QtGui.QApplication(sys.argv)
-   #strAddress = "C:\\" # VERY IMPORTANT: Address MUST be finished with "\\". MUST!!
-   listDrives = drivers() # VERY IMPORTANT: Address MUST be finished with "\\". MUST!!
-   print listDrives
-   MainWindow = QtGui.QMainWindow()
-   ui = Ui_MainWindow()
-   ui.setupUi(MainWindow)
-   ui.treeWidget.setHeaderLabels(["Directories"])
-   model0 = QtGui.QFileSystemModel()
-   model0.setRootPath("/")
-   #ui.listView.setModel(model0)
-   #root = QtGui.QTreeWidgetItem(ui.treeWidget)
-   #root.setText(0,"C")
-   #treeView(strAddress,root)
-   listRoot = []
-   for driver in listDrives:
-      tree_widget_item = QtGui.QTreeWidgetItem(ui.treeWidget)
-      list_widget_item = QtGui.QListWidgetItem(ui.listView)
-      list_widget_item.setText(driver[0].upper())
-      icon = QtGui.QIcon('icons/driver.ico')
-      list_widget_item.setIcon(icon)
-      tree_widget_item.setText(0, driver[0].upper())
-      tree_widget_item.setIcon(0, icon)
-      tree_widget_item.setIcon(1, icon)
-      treeView(driver, tree_widget_item)
+    app = QtGui.QApplication(sys.argv)
+    # strAddress = "C:\\" # VERY IMPORTANT: Address MUST be finished with "\\". MUST!!
+    listDrives = drivers()     # VERY IMPORTANT: Address MUST be finished with "\\". MUST!!
+    print listDrives
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.treeWidget.setHeaderLabels(["Directories"])
+    model0 = QtGui.QFileSystemModel()
+    model0.setRootPath("/")
+    # ui.listView.setModel(model0)
+    # root = QtGui.QTreeWidgetItem(ui.treeWidget)
+    # root.setText(0,"C")
+    # treeView(strAddress,root)
+    list_root = []
+    for driver in listDrives:
+       tree_widget_item = QtGui.QTreeWidgetItem(ui.treeWidget)
+       list_widget_item = QtGui.QListWidgetItem(ui.listView)
+       list_widget_item.setText(driver[0].upper())
+       icon = QtGui.QIcon('icons/driver.ico')
+       list_widget_item.setIcon(icon)
+       tree_widget_item.setText(0, driver[0].upper())
+       tree_widget_item.setIcon(0, icon)
+       tree_widget_item.setIcon(1, icon)
+       treeView(driver, tree_widget_item)
 
-   MainWindow.show()
-   sys.exit(app.exec_())
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 if __name__=="__main__":
     window()
---
+++ events.py
@@ -0,0 +1,57 @@
+from __Classes import *
+from DefaultUI import *
+from PyQt4 import QtGui
+from funcs import *
+from os import *
+
+def mFileAcNDirectory(folderName, currentDirectory):
+    mkdir(currentDirectory+folderName)
+
+def mFileAcNFile(filename,type,currentDirectory):
+    pass
+
+def mEditAcCopy_triggered():
+    pass
+
+def mEditAcCut_triggered():
+    pass
+
+def mEditAcPaste_triggered():
+    pass
+
+def mEditAcDelete_triggered():
+    pass
+
+def mEditAcUndo_triggered():
+    pass
+
+def mEditAcFind_triggered():
+    pass
+
+def mHelpAcAbout_triggered():
+   msg = QtGui.QMessageBox()
+   msg.setIcon(QtGui.QMessageBox.Information)
+   msg.setWindowTitle("About AP file manager...")
+   msg.setText('<b>This is program was developed as the first AP Project.</b>'
+               '<br> Git Repository:<a href="https://github.com/mrbahrani/File-Managment-AP-Project">'
+               'https://github.com/mrbahrani/File-Managment-AP-Project</a>')
+   #msg.setInformativeText("This is additional information")
+   #msg.setWindowTitle("MessageBox demo")
+   #msg.setDetailedText("The details are as follows:")
+   #msg.setStandardButtons(QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel)
+   #msg.buttonClicked.connect(msgbtn)
+   msg.exec_()
+def mHelpAcHelp_triggered():
+    pass
+
+
+def btnFor_clicked():
+    pass
+
+def btnBack_clicked():
+    pass
+
+def btnUp_clicked():
+    pass
+
+
